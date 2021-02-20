cf1faa0230d260b3424bd1bd039419ce90362c9c
DanyGordon <52357936+DanyGordon@users.noreply.github.com>
2021-02-21 01:41:48 +0200

    Cleare config.js

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 // Configurate your server here
 module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/testDataBase',
+    'secretKey': '',
+    'mongoUrl' : '',
     'facebook' : {
-        clientID: '120439021965709',
-        clientSecret: '46209c6e76827f6e48cd84d4f5166f27'
+        clientID: '',
+        clientSecret: ''
     }
 }
